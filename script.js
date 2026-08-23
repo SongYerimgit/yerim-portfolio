0641a20a11d38683cf0b8e8bd9ce2055b8b6111a
songyerim <syl2549@naver.com>
2026-08-23 20:42:04 +0900

    image update

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const themeToggleBtn = document.getElementById('theme-toggle');
-  const icon = themeToggleBtn.querySelector('.icon');
+    const themeToggleBtn = document.getElementById('theme-toggle');
+    const icon = themeToggleBtn.querySelector('.icon');
 
-  // 이전 선택한 테마가 있다면 가져오고, 없으면 시스템 설정 확인
-  const savedTheme = localStorage.getItem('theme');
-  const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    // 이전 선택한 테마가 있다면 가져오고, 없으면 시스템 설정 확인
+    const savedTheme = localStorage.getItem('theme');
+    const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-  if (savedTheme === 'dark' || (!savedTheme && systemPrefersDark)) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    icon.textContent = '☀️';
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
-    icon.textContent = '🌙';
-  }
-
-  // 버튼 클릭 시 테마 전환 로직
-  themeToggleBtn.addEventListener('click', () => {
-    const currentTheme = document.documentElement.getAttribute('data-theme');
-    let targetTheme = 'light';
-
-    if (currentTheme !== 'dark') {
-      targetTheme = 'dark';
-      icon.textContent = '☀️';
+    if (savedTheme === 'dark' || (!savedTheme && systemPrefersDark)) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        icon.textContent = '☀️';
     } else {
-      icon.textContent = '🌙';
+        document.documentElement.setAttribute('data-theme', 'light');
+        icon.textContent = '🌙';
     }
 
-    document.documentElement.setAttribute('data-theme', targetTheme);
-    localStorage.setItem('theme', targetTheme); // 브라우저 새로고침 시에도 유지
-  });
+    // 버튼 클릭 시 테마 전환 로직
+    themeToggleBtn.addEventListener('click', () => {
+        const currentTheme = document.documentElement.getAttribute('data-theme');
+        let targetTheme = 'light';
+
+        if (currentTheme !== 'dark') {
+            targetTheme = 'dark';
+            icon.textContent = '☀️';
+        } else {
+            icon.textContent = '🌙';
+        }
+
+        document.documentElement.setAttribute('data-theme', targetTheme);
+        localStorage.setItem('theme', targetTheme); // 브라우저 새로고침 시에도 유지
+    });
 });
+
+function copyEmail(email) {
+    // 클립보드 복사 API 실행
+    navigator.clipboard.writeText(email).then(() => {
+        const toast = document.getElementById('toast');
+
+        // 토스트 팝업 띄우기
+        toast.classList.add('show');
+
+        // 2초 뒤에 자동으로 사라지게 설정
+        setTimeout(() => {
+            toast.classList.remove('show');
+        }, 2000);
+    }).catch(err => {
+        console.error('복사 실패:', err);
+    });
+}
